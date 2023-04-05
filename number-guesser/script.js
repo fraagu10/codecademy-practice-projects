f90de6c8c31cb90d418738b1f7d6df6970212a86
fraagu10 <aguilarfranco1092@protonmail.com>
2023-04-05 11:26:38 -0500

    added functions to provide functionality to game

--- number-guesser/script.js
+++ number-guesser/script.js
@@ -2,5 +2,25 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => Math.floor(Math.random() * 9);
+
+const compareGuesses = function(human, computer, target) {
+    if(Math.abs(target - human) <= Math.abs(target - computer)) {
+        return true;
+    }
+
+    return false;
+}
+
+function updateScore(winner) {
+    if(winner === "human") {
+        humanScore++;
+    } else if(winner === "computer") {
+        computerScore++;
+    } 
+}
+
+function advanceRound() {
+    currentRoundNumber++;
+}
 
